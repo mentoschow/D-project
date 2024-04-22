b208e9ab315f43f248fc3e042f68182c1c8a24ec
mentoschow <408756434chow@gmail.com>
2024-04-22 21:57:01 +0800

    添加微信界面

--- Assets/Scripts/Base/GameRes.cs
+++ Assets/Scripts/Base/GameRes.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameRes", menuName = "ScriptableObjects/GameRes", order = 1)]
 public class GameRes : ScriptableObject
 {
-    public List<ImageRes> imageRes = new List<ImageRes>();
+    [Header("线索道具图片资源")]
+    public List<ImageRes> clueItemImage = new List<ImageRes>();
+    [Header("对话中的图片资源")]
+    public List<ImageRes> dialogImage = new List<ImageRes>();
 }
 
 [System.Serializable]
 public class ImageRes
 {
-    public string name;
+    public string ID;
     public Sprite sprite;
 }
