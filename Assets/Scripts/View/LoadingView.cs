e4ce097f426288d5626a32dc0ce3bb3f245a99e9
mentoschow <408756434chow@gmail.com>
2024-05-02 23:01:00 +0800

    添加转场逻辑

--- Assets/Scripts/View/LoadingView.cs
+++ Assets/Scripts/View/LoadingView.cs
@@ -30,7 +30,42 @@ public class LoadingView : MonoBehaviour
 
     public void PlayTransition(TransitionType type)
     {
-
+        switch (type)
+        {
+            case TransitionType.Blackout:
+            case TransitionType.None:
+                content.text = "";
+                break;
+            case TransitionType.ChangeToBoy:
+                content.text = "密室（过去）";
+                RoleController.Instance.ChangeRole(RoleType.MainRoleBoy);
+                SceneController.Instance.ChangeScene(StageType.SecretRoom_Pass, StageType.BoxRoom, false);
+                break;
+            case TransitionType.ChangeToGirl:
+                content.text = "藏间（现在）";
+                RoleController.Instance.ChangeRole(RoleType.MainRoleGirl);
+                SceneController.Instance.ChangeScene(StageType.BoxRoom, StageType.SecretRoom_Pass, false);
+                break;
+            case TransitionType.ChangeToBoy2:
+                content.text = "密室（过去）";
+                RoleController.Instance.ChangeRole(RoleType.MainRoleBoy);
+                SceneController.Instance.ChangeScene(StageType.SecretRoom_Pass, StageType.SecretRoom_Now, false);
+                break;
+            case TransitionType.ChangeToGirl2:
+                content.text = "密室（现在）";
+                RoleController.Instance.ChangeRole(RoleType.MainRoleGirl);
+                SceneController.Instance.ChangeScene(StageType.SecretRoom_Now, StageType.SecretRoom_Pass, false);
+                break;
+            case TransitionType.ChangeToLibraryOut:
+                content.text = "图书馆外";
+                SceneController.Instance.ChangeScene(StageType.LibraryOut, StageType.SecretRoom_Now, false);
+                break;
+            case TransitionType.ChangeToSecretRoom_Now:
+                content.text = "密室（现在）";
+                SceneController.Instance.ChangeScene(StageType.SecretRoom_Now, StageType.LibraryOut, false);
+                break;
+        }
+        Invoke("PlayTransitionOver", time);
     }
 
     private void PlayTransitionOver()
